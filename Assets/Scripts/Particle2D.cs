1ea8b4c0251e8ca7a493aa3200de2ce467f04502
Owen Stanclift <owen.stanclift@mymail.champlain.edu>
2024-09-09 10:49:38 -0400

    Velocity is implemented

--- Assets/Scripts/Particle2D.cs
+++ Assets/Scripts/Particle2D.cs
@@ -1,6 +1,18 @@
+using log4net.Util;
 using UnityEngine;
 
 public class Particle2D : MonoBehaviour
 {
+    public int mass;
+    public Vector2 velocity;
+    public Vector2 acceleration;
+    public Vector2 accumulatedForce;
+    public int dampingConstant;
+
+    private void FixedUpdate()
+    {
+        Integrator.integrate(this);
+    }
+
     // TODO: YOUR CODE HERE!
 }
--- Assets/Scripts/ParticleSpawner.cs
+++ Assets/Scripts/ParticleSpawner.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem;
 
 public class ParticleSpawner : MonoBehaviour
 {
     // TODO: YOUR CODE HERE!
+    public Particle2D particle;
+    private Vector2 mousePos;
+
+    private void Start()
+    {
+        Time.timeScale = 0.5f;
+    }
+    private void Update()
+    {
+
+   
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+        }
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            Particle2D newParticle = particle;
+            Vector2 end = (Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+            newParticle.velocity = end - mousePos;
+            Instantiate(newParticle.gameObject, new Vector3(mousePos.x, mousePos.y, 5), Quaternion.identity, this.gameObject.transform);
+            Debug.Log(mousePos);
+            Debug.Log(end);
+
+        }
+    }
 }
